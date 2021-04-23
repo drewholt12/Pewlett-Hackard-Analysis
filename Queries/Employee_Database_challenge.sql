e3eae8538db971ee05945481f38b49ad8484f05a
Andrew Holt <drewholt12@comcast.net>
2021-04-23 15:41:30 -0500

    added mentorship csv and query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,3 +29,22 @@ INTO retiring_table
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2 Create mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no)  -- selecting the columns we want
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_info  --  creating new table
+FROM employees as e  -- 1st table to join and alias
+INNER JOIN dept_emp as de  -- type of join with 2nd table and alias
+ON (e.emp_no = de.emp_no) -- column that is equal
+LEFT JOIN titles as ti  -- left join because the from and to_date columns are different between titles and dept_emp which causes errors. and alias
+on (e.emp_no = ti.emp_no)  -- colum that is equal
+WHERE (e.birth_date  BETWEEN '1965-01-01' AND '1965-12-31')  -- limiting to birth years
+	AND (de.to_date = '9999-01-01') -- limits to current employee date code
+ORDER BY emp_no;  -- sets order ascending by employee number
